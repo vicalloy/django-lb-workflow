3bd605f350c2a208d683ef3e419aafcb836224ec
vicalloy <zbirder@gmail.com>
2017-04-12 22:08:55 +0800

    add third part APPS settings to tests/settings.py

--- lbworkflow/tests/settings.py
+++ lbworkflow/tests/settings.py
@@ -37,7 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'crispy_forms',
     'lbattachment',
+    'lbadminlte',
+    'lbutils',
+    'compressor',
+    'djangobower',
 
     'lbworkflow',
     'lbworkflow.tests.leave',
@@ -125,3 +130,44 @@ STATIC_URL = '/static/'
 LBWF_APPS = {
     'leave': 'lbworkflow.tests.leave',
 }
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'collectedstatic')
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
+
+CRISPY_TEMPLATE_PACK = 'bootstrap3'
+
+# bower
+STATICFILES_FINDERS += (('djangobower.finders.BowerFinder'),)
+BOWER_COMPONENTS_ROOT = BASE_DIR
+
+BOWER_INSTALLED_APPS = (
+    'admin-lte#2.3.11',
+    'font-awesome#4.7.0',
+    'ionicons#2.0.1',
+
+    'modernizr',
+    # POLYFILLS: javascript fallback solutions for older browsers.
+    # CSS3 selectors for IE 6-8.
+    'selectivizr',
+    # min/max width media queries for IE 6-8.
+    'respond',
+    # CSS3 styles for IE 6-8.
+    'pie',
+    # HTML5 tag support for IE 6-8.
+    'html5shiv',
+)
+
+# django-compressor
+STATICFILES_FINDERS += (('compressor.finders.CompressorFinder'),)
+COMPRESS_PRECOMPILERS = (
+    ('text/coffeescript', 'coffee --compile --stdio'),
+    ('text/less', 'lessc {infile} {outfile}'),
+    ('text/x-sass', 'sass {infile} {outfile}'),
+    ('text/x-scss', 'sass --scss {infile} {outfile}'),
+)
+
+PROJECT_TITLE = 'LB-Workflow'
