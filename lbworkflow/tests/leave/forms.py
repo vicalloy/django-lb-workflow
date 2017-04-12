d3f94e07fa05f237a64099fd86496fcfc3b049dd
vicalloy <zbirder@gmail.com>
2017-04-12 22:10:18 +0800

    change default form name from main_form to form.

--- lbworkflow/tests/leave/forms.py
+++ lbworkflow/tests/leave/forms.py
@@ -2,11 +2,26 @@
 from django import forms
 
 from lbworkflow.forms import WorkflowFormMixin
+from lbutils import FormHelperMixin
+from lbutils import row_div
+from crispy_forms.layout import Layout
 
 from .models import Leave
 
 
-class LeaveForm(WorkflowFormMixin, forms.ModelForm):
+class LeaveForm(FormHelperMixin, WorkflowFormMixin, forms.ModelForm):
+
+    def __init__(self, *args, **kw):
+        super(LeaveForm, self).__init__(*args, **kw)
+        self.one_row_fields = ['reason', ]
+        self.init_crispy_helper()
+        self.helper.label_class = 'col-xs-4'
+        self.helper.layout = Layout(
+            row_div(['start_on', 'end_on'], 6),
+            row_div(['leave_days', ], 6),
+            row_div(['reason'], 12),
+        )
+
     class Meta:
         model = Leave
-        exclude = []
+        fields = ['start_on', 'end_on', 'leave_days', 'reason', ]
--- lbworkflow/tests/leave/views.py
+++ lbworkflow/tests/leave/views.py
@@ -1,5 +1,5 @@
 from lbworkflow.views.generics import CreateView
-from lbworkflow.views.generics import ListView
+from lbworkflow.views.generics import WFListView
 from lbworkflow.views.generics import UpdateView
 
 from .forms import LeaveForm
@@ -7,7 +7,7 @@ from .forms import LeaveForm
 
 class LeaveCreateView(CreateView):
     form_classes = {
-        'main_form': LeaveForm,
+        'form': LeaveForm,
     }
 
 new = LeaveCreateView.as_view()
@@ -15,13 +15,13 @@ new = LeaveCreateView.as_view()
 
 class LeaveUpdateView(UpdateView):
     form_classes = {
-        'main_form': LeaveForm,
+        'form': LeaveForm,
     }
 
 edit = LeaveUpdateView.as_view()
 
 
-class LeaveListView(ListView):
+class LeaveListView(WFListView):
     pass
 
 show_list = LeaveListView.as_view()
