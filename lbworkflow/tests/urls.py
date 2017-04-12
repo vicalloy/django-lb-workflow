4d1ee1efd26eb0f242e8930ccc9c306dc7f33827
vicalloy <zbirder@gmail.com>
2017-04-12 22:05:22 +0800

    add admin to tests/urls.py

--- lbworkflow/tests/urls.py
+++ lbworkflow/tests/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import include
 from django.conf.urls import url
+from django.contrib import admin
 
 urlpatterns = [
+    url(r'^admin/', admin.site.urls),
     url(r'^wf/', include('lbworkflow.urls')),
 ]
