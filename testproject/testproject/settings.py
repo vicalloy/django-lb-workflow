5e8aed2375b6573efe6f5262aa042c3832d77d89
vicalloy <zbirder@gmail.com>
2017-04-13 21:27:40 +0800

    remove BASE_DIR

--- testproject/testproject/settings.py
+++ testproject/testproject/settings.py
@@ -1,12 +1,6 @@
 from lbworkflow.tests.settings import *  # NOQA
 
-import os
-
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 INSTALLED_APPS += ['testproject']
-
 
 DATABASES = {
     'default': {
