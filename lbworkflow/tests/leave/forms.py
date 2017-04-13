76c305b7e09778f3f4402f2e8ece8deaec026f6b
vicalloy <zbirder@gmail.com>
2017-04-13 21:29:33 +0800

    add layout to LeaveForm

--- lbworkflow/tests/leave/forms.py
+++ lbworkflow/tests/leave/forms.py
@@ -2,25 +2,21 @@
 from django import forms
 
 from lbworkflow.forms import WorkflowFormMixin
-from lbutils import FormHelperMixin
-from lbutils import row_div
-from crispy_forms.layout import Layout
+from lbutils import BootstrapFormHelperMixin
 
 from .models import Leave
 
 
-class LeaveForm(FormHelperMixin, WorkflowFormMixin, forms.ModelForm):
+class LeaveForm(BootstrapFormHelperMixin, WorkflowFormMixin, forms.ModelForm):
 
     def __init__(self, *args, **kw):
         super(LeaveForm, self).__init__(*args, **kw)
-        self.one_row_fields = ['reason', ]
         self.init_crispy_helper()
-        self.helper.label_class = 'col-xs-4'
-        self.helper.layout = Layout(
-            row_div(['start_on', 'end_on'], 6),
-            row_div(['leave_days', ], 6),
-            row_div(['reason'], 12),
-        )
+        self.layout_fields([
+            ['start_on', 'end_on'],
+            ['leave_days', ''],
+            ['reason', ],
+        ])
 
     class Meta:
         model = Leave
