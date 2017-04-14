2fdc9b55281250f7ab5875cee6e3e9e53516722d
vicalloy <zbirder@gmail.com>
2017-04-14 21:48:02 +0800

    add test_process.py

---
+++ lbworkflow/tests/test_flowchart.py
@@ -0,0 +1,10 @@
+from django.core.urlresolvers import reverse
+
+from .test_base import BaseTests
+
+
+class ViewTests(BaseTests):
+
+    def test_flowchart(self):
+        resp = self.client.get(reverse('wf_process_flowchart', args=('leave', )))
+        self.assertEqual(resp.status_code, 200)
