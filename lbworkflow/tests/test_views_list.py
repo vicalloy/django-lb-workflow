518cbba1e5b6b25568205be6d299b59d9ccd6cca
vicalloy <zbirder@gmail.com>
2017-04-14 21:48:22 +0800

    add test_views_list.py

---
+++ lbworkflow/tests/test_views_list.py
@@ -0,0 +1,21 @@
+from django.core.urlresolvers import reverse
+
+from .test_base import BaseTests
+
+
+class ViewTests(BaseTests):
+
+    def test_my_wf(self):
+        self.client.login(username='owner', password='password')
+        resp = self.client.get(reverse('wf_my_wf'))
+        self.assertEqual(resp.status_code, 200)
+
+    def test_list_wf(self):
+        self.client.login(username='owner', password='password')
+        resp = self.client.get(reverse('wf_list_wf'))
+        self.assertEqual(resp.status_code, 200)
+
+    def test_todo(self):
+        self.client.login(username='owner', password='password')
+        resp = self.client.get(reverse('wf_todo'))
+        self.assertEqual(resp.status_code, 200)
