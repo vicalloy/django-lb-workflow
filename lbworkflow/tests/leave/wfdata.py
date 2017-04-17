958312b5ce1a909691364560d12b2747b2e23a9e
vicalloy <zbirder@gmail.com>
2017-04-17 22:37:21 +0800

    update leave/wfdata.py

--- lbworkflow/tests/leave/wfdata.py
+++ lbworkflow/tests/leave/wfdata.py
@@ -1,13 +1,15 @@
 from lbworkflow.core.datahelper import create_process
 from lbworkflow.core.datahelper import create_activity
+from lbworkflow.core.datahelper import create_category
 from lbworkflow.core.datahelper import create_transition
 from lbworkflow.core.datahelper import create_app
 
 
 def load_data(wf_code):
     create_app('5f31d065-00aa-0010-beea-641f0a670010', 'Simple URL', action='wf_execute_transition')
+    category = create_category('5f31d065-00cc-0010-beea-641f0a670010', 'HR')
 
-    process = create_process('leave', 'Leave')
+    process = create_process('leave', 'Leave', category=category)
     create_activity('5f31d065-00a0-0010-beea-641f0a670010', process, 'Draft', status='draft')
     create_activity('5f31d065-00a0-0010-beea-641f0a670020', process, 'Given up', status='given up')
     create_activity('5f31d065-00a0-0010-beea-641f0a670030', process, 'Rejected', status='rejected')
