70d06c3379936cbd5330efea7411ec2a48d6e424
vicalloy <zbirder@gmail.com>
2017-04-17 15:32:38 +0800

    fix migrations dependencies

--- lbworkflow/migrations/0001_initial.py
+++ lbworkflow/migrations/0001_initial.py
@@ -16,7 +16,7 @@ class Migration(migrations.Migration):
     dependencies = [
         ('contenttypes', '0002_remove_content_type_name'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('lbattachment', '0002_auto_20170401_0328'),
+        ('lbattachment', '0001_initial'),
     ]
 
     operations = [
