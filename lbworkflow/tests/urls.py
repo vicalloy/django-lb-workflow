3d8a8f59b8631ebdd6fbcb65cce2e7e825f9b9eb
vicalloy <zbirder@gmail.com>
2017-04-17 22:49:43 +0800

    redirect home to /wf/list/

--- lbworkflow/tests/urls.py
+++ lbworkflow/tests/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import include
 from django.conf.urls import url
 from django.contrib import admin
+from django.views.generic import RedirectView
+
 
 urlpatterns = [
+    url(r'^$', RedirectView.as_view(url='/wf/list/'), name='home'),
     url(r'^admin/', admin.site.urls),
     url(r'^wf/', include('lbworkflow.urls')),
 ]
