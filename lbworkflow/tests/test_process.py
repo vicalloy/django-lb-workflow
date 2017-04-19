56553be9f51a14bb992e58f32973892e8d5b7ea3
vicalloy <zbirder@gmail.com>
2017-04-19 14:27:14 +0800

    add test code for submit and update workflow

--- lbworkflow/tests/test_process.py
+++ lbworkflow/tests/test_process.py
@@ -3,6 +3,7 @@ from django.core.urlresolvers import reverse
 
 from lbworkflow.views.helper import user_wf_info_as_dict
 
+from .leave.models import Leave
 from .test_base import BaseTests
 
 User = get_user_model()
@@ -34,9 +35,6 @@ class ViewTests(BaseTests):
         resp = self.client.get(reverse('wf_start_wf'))
         self.assertEqual(resp.status_code, 200)
 
-        resp = self.client.get(reverse('wf_new', args=('leave', )))
-        self.assertEqual(resp.status_code, 200)
-
         resp = self.client.get(reverse('wf_list', args=('leave', )))
         self.assertEqual(resp.status_code, 200)
 
@@ -46,3 +44,54 @@ class ViewTests(BaseTests):
         resp = self.client.get(reverse('wf_detail', args=('1', )))
         self.assertEqual(resp.status_code, 200)
         self.assertContains(resp, 'from leave.detail')
+
+    def test_submit(self):
+        self.client.login(username='owner', password='password')
+
+        url = reverse('wf_new', args=('leave', ))
+        resp = self.client.get(url)
+        self.assertEqual(resp.status_code, 200)
+
+        data = {
+            'start_on': '2017-04-19 09:01',
+            'end_on': '2017-04-20 09:01',
+            'leave_days': '1',
+            'reason': 'test save',
+        }
+        resp = self.client.post(url, data)
+        leave = Leave.objects.get(reason='test save')
+        self.assertRedirects(resp, '/wf/%s/' % leave.pinstance.pk)
+        self.assertEqual('Draft', leave.pinstance.cur_activity.name)
+
+        data['act_submit'] = 'Submit'
+        data['reason'] = 'test submit'
+        resp = self.client.post(url, data)
+        leave = Leave.objects.get(reason='test submit')
+        self.assertRedirects(resp, '/wf/%s/' % leave.pinstance.pk)
+        self.assertEqual('A2', leave.pinstance.cur_activity.name)
+
+    def test_edit(self):
+        self.client.login(username='owner', password='password')
+
+        data = {
+            'start_on': '2017-04-19 09:01',
+            'end_on': '2017-04-20 09:01',
+            'leave_days': '1',
+            'reason': 'test save',
+        }
+        url = reverse('wf_new', args=('leave', ))
+        resp = self.client.post(url, data)
+        leave = Leave.objects.get(reason='test save')
+        self.assertRedirects(resp, '/wf/%s/' % leave.pinstance.pk)
+        self.assertEqual('Draft', leave.pinstance.cur_activity.name)
+
+        url = reverse('wf_edit', args=(leave.pinstance.pk, ))
+        resp = self.client.get(url)
+        self.assertEqual(resp.status_code, 200)
+
+        data['act_submit'] = 'Submit'
+        data['reason'] = 'test submit'
+        resp = self.client.post(url, data)
+        leave = Leave.objects.get(reason='test submit')
+        self.assertRedirects(resp, '/wf/%s/' % leave.pinstance.pk)
+        self.assertEqual('A2', leave.pinstance.cur_activity.name)
