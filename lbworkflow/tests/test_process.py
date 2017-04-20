77d7367e0ada59c03385729fe40f93575f777851
vicalloy <zbirder@gmail.com>
2017-04-20 20:58:31 +0800

    add test for start_wf and detail

--- lbworkflow/tests/test_process.py
+++ lbworkflow/tests/test_process.py
@@ -31,16 +31,19 @@ class HelperTests(BaseTests):
 
 class ViewTests(BaseTests):
 
-    def test_base_get(self):
+    def setUp(self):
+        super(ViewTests, self).setUp()
+        self.client.login(username='owner', password='password')
+
+    def test_start_wf(self):
         resp = self.client.get(reverse('wf_start_wf'))
         self.assertEqual(resp.status_code, 200)
 
+    def test_wf_list(self):
         resp = self.client.get(reverse('wf_list', args=('leave', )))
         self.assertEqual(resp.status_code, 200)
 
-        resp = self.client.get(reverse('wf_edit', args=('1', )))
-        self.assertEqual(resp.status_code, 200)
-
+    def test_detail(self):
         resp = self.client.get(reverse('wf_detail', args=('1', )))
         self.assertEqual(resp.status_code, 200)
         self.assertContains(resp, 'from leave.detail')
