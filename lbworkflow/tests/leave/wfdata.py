9aad23528d172bd8c6e80b8d10fc2406e6ade3ed
vicalloy <zbirder@gmail.com>
2017-04-21 19:22:45 +0800

    add data for create_activity update test

--- lbworkflow/tests/leave/wfdata.py
+++ lbworkflow/tests/leave/wfdata.py
@@ -12,6 +12,7 @@ def load_data(wf_code):
 
     process = create_process('leave', 'Leave', category=category)
     create_activity('5f31d065-00a0-0010-beea-641f0a670010', process, 'Draft', status='draft')
+    create_activity('5f31d065-00a0-0010-beea-641f0a670010', process, 'Draft', status='draft')  # test for update
     create_activity('5f31d065-00a0-0010-beea-641f0a670020', process, 'Given up', status='given up')
     create_activity('5f31d065-00a0-0010-beea-641f0a670030', process, 'Rejected', status='rejected')
     create_activity('5f31d065-00a0-0010-beea-641f0a670040', process, 'Completed', status='completed')
