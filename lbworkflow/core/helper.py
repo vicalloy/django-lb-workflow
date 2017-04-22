cd77ad72f3cb228b716a1dfbbea37e2c70557fe5
vicalloy <zbirder@gmail.com>
2017-04-22 23:33:09 +0800

    add as_class

--- lbworkflow/core/helper.py
+++ lbworkflow/core/helper.py
@@ -5,12 +5,16 @@ from django.db.models import Q
 from lbworkflow import settings
 
 
-def as_func(func):
-    if callable(func):
-        return func
-    idx = func.rindex(r'.')
-    _module = importlib.import_module(func[:idx])
-    return getattr(_module, func[idx + 1:])
+def as_func(func_path):
+    if callable(func_path):
+        return func_path
+    idx = func_path.rindex(r'.')
+    _module = importlib.import_module(func_path[:idx])
+    return getattr(_module, func_path[idx + 1:])
+
+
+def as_class(cls_path):
+    return as_func(cls_path)
 
 
 def safe_eval(source, globals, *args, **kwargs):
