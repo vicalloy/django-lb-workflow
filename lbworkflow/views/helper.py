ef26d635a7a8418d99c55fa1435172d371f6f4c5
vicalloy <zbirder@gmail.com>
2017-04-22 22:56:55 +0800

    refactor user_wf_info_as_dict

--- lbworkflow/views/helper.py
+++ lbworkflow/views/helper.py
@@ -31,14 +31,7 @@ def user_wf_info_as_dict(wf_obj, user):
     instance = wf_obj.pinstance
     is_wf_admin = instance.is_wf_admin(user)
     in_process = instance.cur_activity.status == 'in progress'
-    workitems = WorkItem.objects.filter(
-        Q(user=user) | Q(agent_user=user),
-        instance=instance, status='in progress')
-    workitem = workitems.first()
-    transitions = []
-    if workitems.exists():
-        workitems.filter(receive_on=None).update(receive_on=timezone.now())
-        transitions = instance.get_transitions()
+    workitem = instance.get_todo_workitem(user)
     ctx['wf_code'] = instance.process.code
     ctx['process'] = instance.process
     ctx['process_instance'] = instance
@@ -47,18 +40,28 @@ def user_wf_info_as_dict(wf_obj, user):
     ctx['wf_history'] = instance.event_set.all().order_by('-created_on', '-pk')
     ctx['operators_display'] = instance.get_operators_display()
     ctx['is_wf_admin'] = is_wf_admin
-    ctx['can_assign'] = in_process and (workitem or is_wf_admin or user.is_superuser)
-    ctx['can_remind'] = in_process and (instance.created_by == user or is_wf_admin)
-    ctx['can_give_up'] = instance.can_give_up(user)
-    can_edit = (instance.cur_activity.status not in ['in progress', 'completed'] and instance.created_by == user)
+
+    can_edit = not instance.cur_activity.is_submitted() and instance.created_by == user
     can_edit = can_edit or (instance.cur_activity.can_edit and workitem)
     can_edit = can_edit or is_wf_admin
     ctx['can_edit'] = can_edit
-    ctx['can_audit'] = instance.get_todo_workitem(user)
     ctx['can_rollback'] = instance.can_rollback(user)
-    ctx['transitions'] = transitions
-    ctx['agree_transitions'] = instance.get_merged_agree_transitions()
-    ctx['other_transitions'] = [e for e in transitions if not e.is_agree]
+
+    if in_process:
+        ctx['can_assign'] = workitem or is_wf_admin or user.is_superuser
+        ctx['can_remind'] = instance.created_by == user or is_wf_admin
+        ctx['can_give_up'] = instance.can_give_up(user)
+
+    if workitem:
+        instance.get_todo_workitems(user).filter(
+            receive_on=None
+        ).update(receive_on=timezone.now())
+        transitions = instance.get_transitions()
+        ctx['can_reject'] = instance.cur_activity.can_reject
+        ctx['can_back_to'] = None
+        ctx['transitions'] = transitions
+        ctx['agree_transitions'] = instance.get_merged_agree_transitions()
+        ctx['other_transitions'] = [e for e in transitions if not e.is_agree]
     # TODO add reject,given up to other_transitions?
     return ctx
 
