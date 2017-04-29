205eba0277de79e40a95f9186e85113e928d72cb
vicalloy <zbirder@gmail.com>
2017-04-29 22:27:33 +0800

    add grouped filed for flown

--- lbworkflow/flowgen/__init__.py
+++ lbworkflow/flowgen/__init__.py
@@ -20,6 +20,13 @@ def get_field_names(model_class):
     return ', '.join(["'%s'" % e.name for e in fields])
 
 
+def group(flat_list):
+    for i in range(len(flat_list) % 2):
+        flat_list.append(None)
+        pass
+    return zip(flat_list[0::2], flat_list[1::2])
+
+
 class FlowAppGenerator(object):
     def __init__(self, app_template_path=None):
         if not app_template_path:
@@ -53,12 +60,14 @@ class FlowAppGenerator(object):
             'wf_name': model_class._meta.verbose_name,
             'field_names': get_field_names(model_class),
             'fields': get_fields(model_class),
+            'grouped_fields': group(get_fields(model_class)),
         }
         if detail_model_class:
             ctx.update({
                 'dm__class_name': detail_model_class.__name__,
                 'dm__field_names': get_field_names(detail_model_class),
                 'dm__fields': get_fields(detail_model_class),
+                'dm__grouped__fields': group(get_fields(detail_model_class)),
             })
         self.copy_template(self.app_template_path, dest, ctx)
 
