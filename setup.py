d023607867ccc61017c3c9a6e2cb3b4d68af8bc0
vicalloy <zbirder@gmail.com>
2017-05-03 21:18:17 +0800

    add requires to setup.py

--- setup.py
+++ setup.py
@@ -12,14 +12,29 @@ setup(
     packages=find_packages(exclude=['tests']),
     include_package_data=True,
     install_requires=[
-        'Django>=1.8',
+        'django>=1.10',
+        'jsonfield>=1.0.1',
+        'pygraphviz>=1.3',
+        'xlsxwriter>=0.9.6',
+        'jinja2>=2.9.6',
+        'django-lbutils>=1.0.3',
+        'django-lbattachment>=1.0.2',
+        'django-stronghold',
     ],
     extras_require={
         'tests': [
             'coverage',
             'flake8>=2.0,<3.0',
             'isort',
-        ]
+        ],
+        'options': [
+            'django-compressor>=2.1.1',
+            'django-bower>=5.2.0',
+            'django-crispy-forms>=1.6',
+            'django-lb-adminlte>=0.9.4',
+            'django-el-pagination>=3.0.1',
+            'django-impersonate',
+        ],
     },
     classifiers=[
         'Development Status :: 3 - Alpha',
