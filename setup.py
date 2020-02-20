4040ae02f0edb46709a69a1649311f3f26c35c22
vicalloy <zbirder@gmail.com>
2020-02-20 15:35:13 +0800

    add python_requires for setup.py

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ setup(
     description="Reusable workflow library for Django",
     license='BSD',
     packages=find_packages(exclude=['tests']),
+    python_requires=">=3.4",
     include_package_data=True,
     install_requires=[
         'django>=1.10',
