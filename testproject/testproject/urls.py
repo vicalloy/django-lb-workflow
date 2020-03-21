b2f1ca8b025b6ef7c5306706cf082f92b394ba47
vicalloy <zbirder@gmail.com>
2020-03-21 09:02:32 +0800

    isort format

--- testproject/testproject/urls.py
+++ testproject/testproject/urls.py
@@ -1,8 +1,7 @@
 from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import include
 from django.urls import path
-from django.conf.urls.static import static
-
 
 urlpatterns = [
     path('', include('lbworkflow.tests.urls')),
